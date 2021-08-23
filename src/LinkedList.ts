b6cb140baaedda7bce1cd514f1ad15361731836f
Alfredo <Alfredo.Antuna@lntinfotech.com>
2021-08-23 15:41:45 -0500

    Stack structure passed all the tests

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,1 +1,3 @@
-export default class LinkedList {}
+export default class LinkedList {
+    
+}
--- src/Stack.ts
+++ src/Stack.ts
@@ -1,1 +1,50 @@
-export default class Stack {}
+import Node from "./Node";
+
+
+
+
+export default class Stack {
+
+
+        stack: Array<Node>;
+        len: number;
+
+        constructor() {
+            this.stack = [];
+            this.len = 0;
+            }
+        
+        //check length variable for comments
+        public isEmpty() : boolean{
+            if(this.len ==0){return true;
+            }else{
+                return false;
+            }
+        }
+        //remove the element at length-1
+        public pop(){
+            this.len--;
+            return this.stack[this.len];
+
+            
+        }
+        //show element at length-1(top of stack)
+        public peek(){
+            return this.stack[this.len-1];
+
+        }
+        // add an element to top of stack(end of array)
+        public push(value: Node){
+            this.stack.push(value);
+            this.len++;
+            
+        }
+        //return length variable
+        public count() : number{
+            return this.len;
+        }
+
+
+
+    
+}
