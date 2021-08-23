dc54652bd1eaf481a85a111d05c8e1806b281c71
Alfredo <Alfredo.Antuna@lntinfotech.com>
2021-08-23 16:35:06 -0500

    Started Linkedlist Unfinished type error

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,3 +1,35 @@
+import Node from "./Node";
+
+
+
 export default class LinkedList {
-    
+    //sentinel : Node;
+    head : Node;
+    list : Array<Node>;
+
+
+
+    constructor(){
+        
+        //this.sentinel = new Node();
+        
+        this.list = [];
+        this.head = this.list[0];
+    }
+
+    public isEmpty(){
+        if(this.list.length == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public add(newNode : Node){
+        this.list.push(newNode);
+    }
+    public next(currentNode : Node){
+        let nextIndex = this.list.indexOf(currentNode) + 1;
+        return this.list[nextIndex];
+
+    }
 }
--- src/Node.ts
+++ src/Node.ts
@@ -1,8 +1,10 @@
 export default class Node {
     private _value: any;
+    private nextNode;
 
     constructor(value: any = null) {
         this._value = value;
+        this.nextNode = null;
     }
 
     public get value() {
@@ -12,4 +14,14 @@ export default class Node {
     public set value(val) {
         this._value = val;
     }
+
+    public setNextNode(otherNode : Node){
+        this.nextNode = otherNode;
+    }
+
+    public next(){
+        return this.nextNode;
+    }
+
+    
 }
